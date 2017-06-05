4445f58b30b67e41505f2d05beac622245cb3a51
jss <jss@localhost.localdomain>
2017-06-05 19:34:48 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontDemoApp
+ * @description
+ * # frontDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontDemoApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontDemoApp
+ */
+angular.module('frontDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
